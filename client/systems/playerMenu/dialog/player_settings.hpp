d133a1a319ebf46e2f5098dd1efb480c4451851a
Isaac Harding <Isaac Harding>
2016-08-04 12:40:35 -0400

    Fixed view distance conflict

--- client/systems/playerMenu/dialog/player_settings.hpp
+++ client/systems/playerMenu/dialog/player_settings.hpp
@@ -158,52 +158,5 @@ class playerSettings {
 			x = 0.158; y = 0.66;
 			w = 0.225; h = 0.033 * safezoneH;
 		};
-		
-		class btnDistanceEffects : w_RscButton {
-			idc = -1;
-			text = "Effects";
-			onButtonClick = "[] execVM 'addons\disableEnvironment\disableEnvironment.sqf'";
-			x = 0.158; y = 0.5;
-			w = 0.125; h = 0.033 * safezoneH;
-		};
-
-		/*class btnDistanceNear : w_RscButton {
-			idc = -1;
-			text = "Near";
-			onButtonClick = "setViewDistance 1100;";
-			x = 0.02; y = 0.43;
-			w = 0.125; h = 0.033 * safezoneH;
-		};
-
-		class btnDistanceMedium : w_RscButton {
-			idc = -1;
-			text = "Medium";
-			onButtonClick = "setViewDistance 2200;";
-			x = 0.02; y = 0.5;
-			w = 0.125; h = 0.033 * safezoneH;
-		};
-
-		class btnDistanceFar : w_RscButton {
-			idc = -1;
-			text = "Far";
-			onButtonClick = "setViewDistance 3300;";
-			x = 0.02; y = 0.57;
-			w = 0.125; h = 0.033 * safezoneH;
-		};
-
-		class btnDistanceInsane : w_RscButton {
-			text = "Insane";
-			onButtonClick = "setViewDistance 5000;";
-			x = 0.02; y = 0.60;
-			w = 0.125; h = 0.033 * safezoneH;
-		};*/
-
-		class btnDistanceCHVD : w_RscButton {
-			idc = -1;
-			text = "Viewdist.";
-			onButtonClick = "call CHVD_fnc_openDialog";
-			x = 0.02; y = 0.57;
-			w = 0.125; h = 0.033 * safezoneH;
-		};
 	};
 };
